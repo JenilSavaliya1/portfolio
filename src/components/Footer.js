a6ddb515c0c4d9eeffcb902a7fa0d9837fcb9d80
jenil <jn569163@dal.ca>
2025-03-21 03:26:41 -0300

    removed unnecessary codes and refactored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import '../Styles/Footer.css'
 import { ThemeContext } from './ThemeContext';
 
 function Footer() {
-  const { theme, setTheme } = useContext(ThemeContext); 
+  const { theme } = useContext(ThemeContext); 
   const color = theme === 'light' ? 'rgb(170, 156, 156)' : 'rgb(40, 37, 37)'; 
-  const changeTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-  }
 
   return (
     <footer style={{ backgroundColor: color, color: theme === 'light' ? 'black' : 'white' }}>
